7ae4de50e72aa7565a8d8980eb58a9984395710c
zhiyu1998 <542716863@qq.com>
2024-03-18 21:49:32 +0800

    🐞 fix: 修正生产环境地址

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://zhiyu1998.github.io/Python-Basis-Notes/docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
